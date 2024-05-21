eb327f47eff9d167304b30619090ec1fddd7d72d
Paulo Xavier <paulohenriquex18@gmail.com>
2024-05-21 20:01:42 -0300

    main

--- calculando_IMC_paciente/js/script.js
+++ calculando_IMC_paciente/js/script.js
@@ -1,11 +1,12 @@
 
+import { Modal } from './modal.js'
+import  { AlertError } from './alert-error.js'
+
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height') 
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+
 
 form.onsubmit = event => {
     event.preventDefault()
@@ -13,14 +14,30 @@ form.onsubmit = event => {
     const weight = inputWeight.value
     const height = inputHeight.value
 
+    const showAlertError = notAnumber(weight) || notAnumber (height)
+        
+    if (showAlertError) {
+        AlertError.open()
+        return;
+    }
+
+    AlertError.close()
+
+
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
     
-    modalMessage.innerText = message
-    modalWrapper.classList.add('open')
+    Modal.message.innerText = message
+    Modal.open()
     
+}
+
+function notAnumber (value) {
+    return isNaN (value) || value == ""
 }
 
 function IMC(weight, height) {
     return (weight / ((height / 100) ** 2)).toFixed(2)
 }
+
+
