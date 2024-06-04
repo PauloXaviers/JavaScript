caa9f7c478652c920a2f23b296f5900261a92c92
Paulo Xavier <paulohenriquex18@gmail.com>
2024-06-04 17:49:02 -0300

    focus_timer

---
+++ focus_timer/FocusTimer/state.js
@@ -0,0 +1,6 @@
+export default {
+    minutes: 0,
+    seconds: 8,
+    isRunning: false,
+    isMute: true
+}
---
+++ focus_timer/main.js
@@ -0,0 +1,8 @@
+import './toggle-modal.js'
+
+import * as FocusTimer from './FocusTimer/index.js'
+
+FocusTimer.start(0, 6)
+
+
+
